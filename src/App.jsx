ac6da748c348f2017a5c82b166ef837caa08f756
Ayush Omar <ayushomar72690@gmail.com>
2024-12-13 18:33:53 +0530

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.css'
+import Chatsection from './Components/chatsection/ChatSection'
+import Seperation from './Components/seperation/seperation' 
+import Sidebar from './Components/sidebar/Sidebar'
+
+function App() {
+ 
+
+  return (
+    <>
+      <Sidebar/>
+      <Seperation/>
+      <Chatsection/>
+    </>
+  )
+}
+
+export default App
